0ac88e3f66b5b7e66e346b4b37062030815c399d
wangzixi-diablo <cle.ee@163.com>
2021-11-26 12:37:44 +0800

    remove unused cms module

--- feature-libs/cart/quick-order/root/quick-order-root.module.ts
+++ feature-libs/cart/quick-order/root/quick-order-root.module.ts
@@ -3,8 +3,7 @@ import { RouterModule } from '@angular/router';
 import {
   provideDefaultConfig,
   provideDefaultConfigFactory,
-  RoutingConfig,
-  CmsModule,
+  RoutingConfig
 } from '@spartacus/core';
 import {
   CmsPageGuard,
